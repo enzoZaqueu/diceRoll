74d3f9ddd941a3e8e905d1b972b2df2b7cc290a6
EnzoZ <enzo.zaqueu@gmail.com>
2023-04-19 18:11:13 -0300

    ajustes feedback para melhor entendimento

--- index.js
+++ index.js
@@ -3,87 +3,69 @@ roll.addEventListener('click', diceRoll);
 function rollNumber() {
     return Math.floor((Math.random() * 6)+1);
 }
-function diceRoll() {
 
-    // cria um número de 1 a 6 para o player 1
-    var diceFaceUm = rollNumber();
+var players = document.querySelectorAll("img");
 
-    //troca a face do dado do Player 1
-    player1.setAttribute("src", `link/dice${diceFaceUm}.png`);
-    player1.classList.add("scaleTransform");
-    setTimeout(function(){
-        player1.classList.remove("scaleTransform")
-    }, 200);
-
-    // cria um número de 1 a 6 para o player 2
-    var diceFaceDois = rollNumber();
-
-    // troca a face do dado do Player 2
-    player2.setAttribute("src", `link/dice${diceFaceDois}.png`);
-    player2.classList.add("scaleTransform");
-    setTimeout(function(){
-        player2.classList.remove("scaleTransform");
-    },200);
-
-    if (diceFaceUm === diceFaceDois) {
-        result.innerHTML = "Empate!";
-    }
-    if (diceFaceUm > diceFaceDois) {
-        result.innerHTML = "Player 1 ganhou!";
-    }
-    if (diceFaceUm < diceFaceDois) {
-        result.innerHTML = "Player 2 ganhou!";
-    }
+function rnd() {
+    var rdnN = Math.random();
+    return rdnN;
 }
 
-    // antigo código
+// animacao inicial
+var i = 0;
+function animInicio() {
+    if (i < 5){
+        setTimeout(function(){
+            for (let index = 0; index < 2; index++) {
+                players[index].style.transform=`rotate(${Math.floor(rnd() * -10) + 5}deg) scale(1.05) translate(${(rnd()*10)-5}px, 5px)`;
+                players[index].setAttribute("src", `link/dice${rollNumber()}.png`)
+            }
+            setTimeout(function(){
+                for (let index = 0; index < 2; index++) {
+                    players[index].style.transform=`rotate(0deg) scale(1) translate(0px, 0px)`;
+                }
+            },75);
+            i++;
+            animInicio();
+        }, 100);
+    }
+}
+animInicio();
+
+function diceRoll() {
+
+    var diceFaceUm = rollNumber();
+    var diceFaceDois = rollNumber();
+
+    player1.setAttribute("src", `link/dice${diceFaceUm}.png`);
+    player2.setAttribute("src", `link/dice${diceFaceDois}.png`);
+
+    for (let index = 0; index < 2; index++) {
+        players[index].style.transform=`rotate(${Math.floor(rnd() * -10) + 5}deg) scale(1.05)`;
+    }
+
+    setTimeout(function(){
+        for (let index = 0; index < 2; index++) {
+            players[index].style.transform=`rotate(0deg) scale(1) translate(0px, 0px)`;
+        }
+    },50);
+
+    setTimeout(function() {
+        result.style.transform="scale(1.05)";
+        if (diceFaceUm === diceFaceDois) {
+            result.innerHTML = "Empate!";
+        }
+        if (diceFaceUm > diceFaceDois) {
+            result.innerHTML = "Player 1 ganhou!";
+        }
+        if (diceFaceUm < diceFaceDois) {
+            result.innerHTML = "Player 2 ganhou!";
+        }
     
-    // switch (diceFaceUm) {
-    //     case 1:
-    //     player1.setAttribute("src", "link/dice1.png")
-    //         break;
-    //     case 2:
-    //     player1.setAttribute("src", "link/dice2.png");
-    //         break;
-    //     case 3:
-    //     player1.setAttribute("src", "link/dice3.png");
-    //         break;
-    //     case 4:
-    //     player1.setAttribute("src", "link/dice4.png");
-    //         break;
-    //     case 5:
-    //     player1.setAttribute("src", "link/dice5.png");
-    //         break;
-    //     case 6:
-    //     player1.setAttribute("src", "link/dice6.png");
-    //         break;
-    //     default:
-    //     player1.setAttribute("src", "link/dice6.png");
-    //         break;
-    // }
+        },100)
+    
+    setTimeout(function() {
+        result.style.transform="scale(1)";
+        },200)
 
-    // antigo codigo
-
-    // switch (diceFaceDois) {
-    //     case 1:
-    //     player2.setAttribute("src", "link/dice1.png")
-    //         break;
-    //     case 2:
-    //     player2.setAttribute("src", "link/dice2.png");
-    //         break;
-    //     case 3:
-    //     player2.setAttribute("src", "link/dice3.png");
-    //         break;
-    //     case 4:
-    //     player2.setAttribute("src", "link/dice4.png");
-    //         break;
-    //     case 5:
-    //     player2.setAttribute("src", "link/dice5.png");
-    //         break;
-    //     case 6:
-    //     player2.setAttribute("src", "link/dice6.png");
-    //         break;
-    //     default:
-    //     player2.setAttribute("src", "link/dice6.png");
-    //         break;
-    // }
+}
